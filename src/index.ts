08be1b6a1d88e1725e4154f369d8469b1b9b0904
Moilanen Tuukka <tuukka.moilanen@edu.espoo.fi>
2024-01-10 23:19:38 +0200

    private methods

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,59 @@
 export class MultipartNetworkRequest {
-    requestInitialized: [any, any] | undefined;
-    requestHandled: [any, any] | undefined;
-    requestFinished: [any, any] | undefined;
-    requestError: number | null;
+    #requestInitialized: [any, any] | undefined;
+    #requestHandled: [any, any] | undefined;
+    #requestFinished: [any, any] | undefined;
+    #requestError: number | null;
 
     constructor() {
-        this.requestError = null;
+        this.#requestError = null;
     }
 
     init(): MultipartNetworkRequest {
-        this.requestInitialized = process.hrtime();
+        if (!process.send) console.warn('Missing parent process for MultipartNetworkRequest analytics')
+        this.#requestInitialized = process.hrtime();
         return this;
     }
 
     onRequestHandled() {
-        this.requestHandled = process.hrtime(this.requestInitialized);
+        this.#requestHandled = process.hrtime(this.#requestInitialized);
     }
 
     onRequestFinished() {
-        this.requestFinished = process.hrtime(this.requestInitialized);
-        this.dispose();
+        this.#requestFinished = process.hrtime(this.#requestInitialized);
+        this.#dispose();
     }
 
     onRequestError(statusCode: number, details?: any) {
-        this.requestHandled = process.hrtime(this.requestInitialized);
-        this.requestFinished = process.hrtime(this.requestInitialized);
-        this.requestError = statusCode;
-        this.dispose();
+        this.#requestHandled = process.hrtime(this.#requestInitialized);
+        this.#requestFinished = process.hrtime(this.#requestInitialized);
+        this.#requestError = statusCode;
+        this.#dispose();
     }
 
-    dispose() {
-        const handleTime = Math.round((this.requestHandled?.at(0) * 1000) + (this.requestHandled?.at(1) / 1000000));
-        const finishTime = Math.round((this.requestFinished?.at(0) * 1000) + (this.requestFinished?.at(1) / 1000000));
+    #dispose() {
+        const handleTime = Math.round((this.#requestHandled?.at(0) * 1000) + (this.#requestHandled?.at(1) / 1000000));
+        const finishTime = Math.round((this.#requestFinished?.at(0) * 1000) + (this.#requestFinished?.at(1) / 1000000));
 
-        const analytics: MultipartNetworkRequestAnalytics = {
-            status: !this.requestError ? RequestStatus.Success : RequestStatus.Error,
-            error: this.requestError ?? undefined,
+        const analytics = new MultipartNetworkRequestAnalytics({
+            status: !this.#requestError ? RequestStatus.Success : RequestStatus.Error,
+            error: this.#requestError ?? undefined,
             handleTime: handleTime,
             processingTime: finishTime - handleTime,
             totalTime: finishTime,
-        }
+        });
 
-        console.log(analytics);
+        if (process.send) process.send(analytics);
     }
+}
+
+export interface AnalyticsPacket {
+    type: AnalyticsPacketType;
+    status: RequestStatus;
+    error?: number;
+    totalTime: number;
+}
+export enum AnalyticsPacketType {
+    MultipartNetworkRequest
 }
 
 export enum RequestStatus {
@@ -50,10 +61,25 @@ export enum RequestStatus {
     Error = 'ERROR'
 }
 
-export interface MultipartNetworkRequestAnalytics {
-    status: RequestStatus,
-    error?: number,
-    handleTime: number,
-    processingTime: number,
-    totalTime: number,
+export class MultipartNetworkRequestAnalytics implements AnalyticsPacket {
+    type = AnalyticsPacketType.MultipartNetworkRequest;
+    status: RequestStatus;
+    error?: number;
+    handleTime: number;
+    processingTime: number;
+    totalTime: number;
+
+    constructor(options: {
+        status: RequestStatus,
+        error?: number,
+        handleTime: number,
+        processingTime: number,
+        totalTime: number,
+    }) {
+        this.status = options.status;
+        this.error = options.error;
+        this.handleTime = options.handleTime;
+        this.processingTime = options.processingTime;
+        this.totalTime = options.totalTime;
+    }
 }
